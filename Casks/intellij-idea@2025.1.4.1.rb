55f5a04ce7ae43ad54e7225a10d017717b6ce8cd
nfi5h <i@nfi5h.com>
2026-06-16 21:24:55 +0800

    add intellij-idea@2025.1.4.1

---
+++ Casks/intellij-idea@2025.1.4.1.rb
@@ -0,0 +1,50 @@
+cask "intellij-idea@2025.1.4.1" do
+  arch arm: "-aarch64"
+
+  version "2025.1.4.1,251.27812.49"
+  sha256 arm:   "dd36c36e7ef1bd14db16bfd9feb85ae37a7eb8bd11e532689567de168adc61dd",
+         intel: "d3ee0f157d3a342efe067d7dd782b0c6ca6ca4bd3c253f93e47a7c713c8de94f"
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.first}#{arch}.dmg"
+  name "IntelliJ IDEA Ultimate"
+  desc "Java IDE by JetBrains"
+  homepage "https://www.jetbrains.com/idea/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release"
+    strategy :json do |json|
+      json["IIU"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "intellij-idea@eap"
+
+  app "IntelliJ IDEA.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/idea.wrapper.sh"
+  binary shimscript, target: "idea"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/IntelliJ IDEA.app/Contents/MacOS/idea' "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Caches/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Logs/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.intellij.plist",
+    "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+    "~/Library/Preferences/jetbrains.idea.*.plist",
+    "~/Library/Saved Application State/com.jetbrains.intellij.savedState",
+  ]
+end
